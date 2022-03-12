afcfc406590a8398557edb37251069b1bb27b0e3
mohd <mohammad.eydi1420@gmail.com>
2022-03-12 19:00:09 +0330

    add urls and views

--- exam/urls.py
+++ exam/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import ExamQuestion.views as v1
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', v1.view_home)
 ]
